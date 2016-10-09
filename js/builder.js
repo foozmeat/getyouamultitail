0fea8e3fd5d71a23fb1887ddd8fb4d21e02d0e78
James Moore <james@panic.com>
2016-10-09 13:53:30 -0700

    added local/remote and log/command

--- js/builder.js
+++ js/builder.js
@@ -22,9 +22,22 @@ var compile_logs = function () {
         log_dict.ssh = logformline.find("#logssh" + idx).val();
 
         log_dict.split = logformline.find("#logsplit" + idx).is(':checked');
+        log_dict.remote = logformline.find("#logremote" + idx).is(':checked');
+        log_dict.comm = logformline.find("#logcomm" + idx).is(':checked');
 
         log_structure.lines[idx] = log_dict;
     });
+
+    for (var i = 0; i < log_structure.lines.length; i++) {
+        var log = log_structure.lines[i];
+
+        $.each(log, function (key, value) {
+            if (!log[key]) {
+                delete log[key];
+            }
+        });
+
+    }
 
     delete log_structure.encoded;
 
@@ -38,7 +51,7 @@ var compile_logs = function () {
     return log_structure;
 };
 
-var script_link = function() {
+var script_link = function () {
     return sprintf("%s//%s%s?d=%s", document.location.protocol, document.location.hostname, document.location.pathname, log_structure.encoded)
 
 };
@@ -47,11 +60,12 @@ var create_command = function () {
 
     compile_logs();
 
-    var base_command = "#!/usr/bin/env bash\n\n";
-    base_command += sprintf("# Made with\n\n# %s\n\nmultitail -m 0 \\\n", script_link());
+    var base_command = "#!/usr/bin/env bash\n";
+    base_command += "# Made with \"Get you a multitail\"\n";
+    base_command += sprintf("# %s\n\n", script_link());
+    base_command += "multitail -m 0 ";
 
     var all_log_commands = "";
-
 
     for (var i = 0; i < log_structure.lines.length; i++) {
 
@@ -66,21 +80,31 @@ var create_command = function () {
             log_commands += sprintf("-ci %s ", log.color);
         }
 
-        if (log.ssh && log.file) {
+        if (log.file) {
+            if (log.remote && !log.comm) {
 
-            log_commands += log.split ? "-l " : "-L ";
-            log_commands += sprintf("'ssh %s \"tail -n 0 -q -f %s\"' ", log.ssh, log.file);
+                log_commands += log.split ? "-l " : "-L ";
+                log_commands += sprintf("'ssh %s \"tail -q -f %s\"' ", log.ssh, log.file);
 
-        } else if (log.file) {
-            log_commands += log.split ? "-i " : "-I ";
+            } else if (log.remote && log.comm) {
+                log_commands += log.split ? "-l " : "-L ";
+                log_commands += sprintf("'ssh %s \"%s\"' ", log.ssh, log.file);
 
-            log_commands += sprintf('%s ', log.file);
+            } else if (!log.remote && !log.comm) {
+                log_commands += log.split ? "-i " : "-I ";
+                log_commands += sprintf('%s ', log.file);
+
+            } else if (!log.remote && log.comm) {
+                log_commands += log.split ? "-l " : "-L ";
+                log_commands += sprintf('%s ', log.file);
+
+            }
 
         }
 
-        if (i + 1 < num_logs) {
-            log_commands += "\\ \n";
-        }
+        // if (i + 1 < num_logs) {
+        //     log_commands += "\\ \n";
+        // }
 
         all_log_commands += log_commands;
 
@@ -95,18 +119,49 @@ var create_command = function () {
 
 };
 
-var update = function () {
+var update = function (evt) {
+
+    updateLogGroup(evt);
     $('#result').text(create_command());
     $('#link a').attr("href", script_link());
+
     // $('.twitter-share-button').attr("data-url", script_link());
+};
+
+var updateLogGroup = function (evt) {
+    if (evt) {
+        evt = $(evt)[0];
+        var ctl = evt.target;
+        var loggroup = $(evt.currentTarget);
+        var datagroup = loggroup.attr("data-group");
+
+        // console.log(ctl);
+        // console.log(loggroup);
+        // console.log(datagroup);
+
+        if (ctl.id == "logremote" + datagroup) {
+
+            var targetGroup = $("#logssh" + datagroup).parents(".input-group");
+
+            if ($(ctl).is(':checked')) {
+                targetGroup.show();
+            } else {
+                targetGroup.hide();
+
+            }
+
+        }
+
+    }
 };
 
 var add_log = function (logline) {
     // console.log("adding");
 
-    var newLog = $('#logformlinetemplate').clone();
+    var newLog = $("#logformlinetemplate").clone();
     newLog[0].id = '';
     newLog[0].style.display = 'block';
+    // console.log($(newLog[0]));
 
     if (logline) {
         $("#loglabel", newLog).val(logline.label);
@@ -114,6 +169,8 @@ var add_log = function (logline) {
         $("#logssh", newLog).val(logline.ssh);
         $("#logfile", newLog).val(logline.file);
         $("#logsplit", newLog).prop("checked", logline.split);
+        $("#logremote", newLog).prop("checked", logline.remote);
+        $("#logcomm", newLog).prop("checked", logline.comm);
     }
 
     newLog.find("*").each(function (idx, node) {
@@ -129,23 +186,25 @@ var add_log = function (logline) {
 
     });
 
+    newLog.attr("data-group", num_logs);
 
     newLog.appendTo('#builder');
     num_logs++;
-    update();
 
-    newLog.change(function () {
-        update();
+    newLog.change(function (e) {
+        update(e);
     });
+
+    newLog.change();
 
 };
 
-var parse_query = function() {
+var parse_query = function () {
     var data = getParameterByName('d');
 
     if (data) {
         var json_string = LZString.decompressFromEncodedURIComponent(data);
-        console.log(json_string);
+        // console.log(json_string);
 
         log_structure = JSON.parse(json_string);
 
