079fbd4672704fe655e4c5fd80005168f4ef37cd
James Moore <james@panic.com>
2016-10-21 17:09:50 -0700

    ja

--- js/builder.js
+++ js/builder.js
@@ -250,12 +250,12 @@ var update_controls = function() {
     if (num_logs() > 1) {
         $(".deletebutton").show();
 
-        // $("#builder").sortable( "enable" )
+        $("#builder").sortable( "enable" )
 
     } else {
         $(".deletebutton").hide();
 
-        // $("#builder").sortable( "disable" )
+        $("#builder").sortable( "disable" )
     }
 
     // $("#builder .logsplit:eq(0)").bootstrapSwitch('disabled', true);
@@ -357,6 +357,10 @@ var add_log = function (logline) {
         dupe_log(e);
     });
 
+    $(".editbutton", newLog).click(function (e) {
+        e.preventDefault();
+    });
+
     newLog.find("*").each(function (idx, node) {
 
         if (node.id) {
@@ -446,14 +450,14 @@ $(document).ready(function () {
     //     offText: "Horizontal"
     // });
 
-    // $("#builder").sortable({
-    //     items: "> .loggroup",
-    //     opacity: 0.75,
-    //     cursor: "move",
-    //     axis: "y",
-    //     stop: function( event, ui ) { update(event)}
-    //
-    // });
+    $("#builder").sortable({
+        items: "> .loggroup",
+        opacity: 0.75,
+        cursor: "move",
+        axis: "y",
+        stop: function( event, ui ) { update(event)}
+
+    });
 
     parse_query();
 
